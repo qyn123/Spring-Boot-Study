4a23e7d289d3e52f7526d267b4dcdb0345b7e42f
qiaoyn <qiaoyn@si-tech.com.cn>
2020-11-12 13:26:51 +0800

    项目搭建

---
+++ spring-boot-01/src/main/java/com/qiaoyn/study/Applicaion.java
@@ -0,0 +1,16 @@
+package com.qiaoyn.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiaoyn
+ * @description
+ * @date 2020/11/12
+ */
+@SpringBootApplication
+public class Applicaion {
+    public static void main(String[] args) {
+        SpringApplication.run(Applicaion.class,args);
+    }
+}
